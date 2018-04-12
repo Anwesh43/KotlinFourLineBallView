3215f2698d4987a5f8dc959ea1f805a1813eec8d
Anwesh43 <anweshthecool0@gmail.com>
2018-04-12 11:16:07 +0530

    adding FourLineBallView in activity

--- app/src/main/java/ui/anwesome/com/kotlinfourlineballview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinfourlineballview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinfourlineballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.fourlineballview.FourLineBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        FourLineBallView.create(this)
     }
 }
